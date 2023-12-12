61693185e6fffbe8967646122931c5f4658dd598
ituitis20-ozaltun19 <73958269+ituitis20-ozaltun19@users.noreply.github.com>
2023-12-12 17:24:45 +0300

    username ve password singleton yapıları eklendi

--- lib/patterns/Singleton.dart
+++ lib/patterns/Singleton.dart
@@ -4,17 +4,53 @@ class EmailSingleton {
   EmailSingleton._();
 
   static EmailSingleton getInstance() {
-    if (_singleton == null) {
-      _singleton = EmailSingleton._();
-    }
+    _singleton ??= EmailSingleton._();
 
     return _singleton!;
   }
 
   String _email = '';
   void setEmail({required String email}) {
-    this._email = email;
+    _email = email;
   }
 
-  String getEmail() => this._email;
+  String getEmail() => _email;
+}
+
+class UsernameSingleton {
+  static UsernameSingleton? _singleton;
+
+  UsernameSingleton._();
+
+  static UsernameSingleton getInstance() {
+   _singleton ??= UsernameSingleton._();
+
+    return _singleton!;
+  }
+
+  String _username = '';
+  void setUsername({required String username}) {
+    _username = username;
+  }
+
+  String getUsername() => _username;
+}
+
+class PasswordSingleton {
+  static PasswordSingleton? _singleton;
+
+  PasswordSingleton._();
+
+  static PasswordSingleton getInstance() {
+    _singleton ??= PasswordSingleton._();
+
+    return _singleton!;
+  }
+
+  String _password = '';
+  void setPassword({required String password}) {
+    _password = password;
+  }
+
+  String getPassword() => _password;
 }
