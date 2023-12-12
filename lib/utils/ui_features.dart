72cab7fd0507f826d6d635c7efd443eef9fd93e8
ituitis20-kafadar19 <73964825+ituitis20-kafadar19@users.noreply.github.com>
2023-12-12 17:34:37 +0300

    Yeni renkler ve text stilleri eklendi

--- lib/utils/ui_features.dart
+++ lib/utils/ui_features.dart
@@ -2,6 +2,16 @@ import 'package:flutter/material.dart';
 
 class CustomColors {
   static Color darkPurple() => Color.fromARGB(255, 123, 31, 162);
-  static Color middlePurple() => Color.fromARGB(255, 156, 39, 176);
+  static Color middlePurple() => Color.fromARGB(255, 149, 44, 231);
   static Color lightPurple() => Color.fromARGB(255, 186, 104, 200);
+  static Color customGrey() => Color.fromARGB(255, 237, 237, 237);
+  static Color darkGrey() => Color.fromARGB(255, 205, 205, 205);
+}
+
+class CustomTextStyles {
+  static TextStyle drawerMenuTextStyle() => TextStyle(
+        color: CustomColors.middlePurple(),
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+      );
 }
