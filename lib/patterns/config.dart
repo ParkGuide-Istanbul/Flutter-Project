f333694b71160db7b95eef90310df32efe72c395
ituitis20-ozaltun19 <73958269+ituitis20-ozaltun19@users.noreply.github.com>
2023-12-12 17:26:00 +0300

    email, password, username, photo configleri eklendi

--- lib/patterns/config.dart
+++ lib/patterns/config.dart
@@ -6,11 +6,38 @@ class Config {
       'https://o11xc731wl.execute-api.eu-central-1.amazonaws.com/dev2/signup';
   static const String _verifyCodeURL =
       'https://o11xc731wl.execute-api.eu-central-1.amazonaws.com/dev2/verifycode';
+  static const String _forgotPasswordURL =
+      'https://o11xc731wl.execute-api.eu-central-1.amazonaws.com/dev2/forgotpassword';
+  static String _username = '';
+  static String _email = '';
+  static String _password = '';
   static String _userToken = '';
+  static String _profilePicture = '';
 
   static String getLoginURL() => _loginURL;
   static String getSignUpURL() => _signUpURL;
   static String getVerifyURL() => _verifyCodeURL;
+  static String getForgotPasswordURL() => _forgotPasswordURL;
+  static String getUsername() => _username;
+  static String getEmail() => _email;
+  static String getPassword() => _password;
+  static String getProfilePicture() => _profilePicture;
+
+  static void setProfilePicture({required String profilePictureURL}) {
+    _profilePicture = profilePictureURL;
+  }
+
+  static void setEmail({required String email}) {
+    _email = email;
+  }
+
+  static void setPassword({required String password}) {
+    _password = password;
+  }
+
+  static void setUsername({required String username}) {
+    _username = username;
+  }
 
   static void setUserToken(String token) {
     _userToken = token;
