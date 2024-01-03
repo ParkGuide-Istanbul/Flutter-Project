a60136247167c0850ef85f07bb642795eaf378a7
ituitis20-ozaltun19 <73958269+ituitis20-ozaltun19@users.noreply.github.com>
2024-01-04 01:12:03 +0300

    quickSearch ve nearestParks için özel http requestler eklendi, tüm requestlere kullanıcı tokeni içeren header eklendi

--- lib/patterns/httpReqs.dart
+++ lib/patterns/httpReqs.dart
@@ -1,15 +1,44 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:park_guide_istanbul/patterns/config.dart';
 
 class HttpRequests {
   final String _URL;
+  final String _userToken = Config.getUserToken();
 
   HttpRequests(this._URL);
 
-  Future<Map<String, dynamic>> getRequest() async {
+  Future<List<String>> quickSearchGetRequest() async {
     try {
-      final response = await http.get(Uri.parse(_URL));
+      final response = await http.get(Uri.parse(_URL), headers: {
+        'Content-Type': 'application/json',
+        'authorization-token': Config.getUserToken()
+      });
+      if (response.statusCode == 200) {
+        List<String> quickSearches = [];
+        var body = json.decode(response.body);
+        print(body);
+        for (int i = 0; i < body.length; i++) {
+          String searchToAdd = body[i]['lat-lng'];
+          quickSearches.add(searchToAdd.replaceAll("-", ","));
+        }
+        print(quickSearches);
+        return quickSearches;
+      } else {
+        print(response.statusCode);
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+
+  Future<dynamic> getRequest() async {
+    try {
+      final response = await http.get(Uri.parse(_URL), headers: {
+        'Content-Type': 'application/json',
+        'authorization-token': Config.getUserToken()
+      });
 
       if (response.statusCode == 200) {
         return json.decode(response.body);
@@ -21,14 +50,45 @@ class HttpRequests {
     }
   }
 
-  Future<Map<String, dynamic>> postRequest(Map<String, dynamic> data) async {
+
+
+  Future<Map<String, dynamic>> postRequest(Map<String, dynamic> data,
+      {bool? header}) async {
     print("post req triggered.");
     try {
       print(data);
       final response = await http.post(
         Uri.parse(_URL),
         body: json.encode(data),
-        headers: {'Content-Type': 'application/json'},
+        headers: header == null
+            ? {'Content-Type': 'application/json'}
+            : {
+                'Content-Type': 'application/json',
+                'authorization-token': Config.getUserToken()
+              },
+      );
+
+      //print(json.decode(response.body));
+
+      if (response.statusCode == 200) {
+        return json.decode(response.body);
+      } else {
+        return {"message": "Operation failed."};
+      }
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+
+  Future<dynamic> nearestParksPostRequest(Map<String, dynamic> data) async {
+    try {
+      final response = await http.post(
+        Uri.parse(_URL),
+        body: json.encode(data),
+        headers: {
+          'Content-Type': 'application/json',
+          'authorization-token': _userToken
+        },
       );
 
       return json.decode(response.body);
