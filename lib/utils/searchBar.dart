1c205e1d6f33dd32206d1e5b65dcc1ba39601545
ituitis20-ozaltun19 <73958269+ituitis20-ozaltun19@users.noreply.github.com>
2024-01-04 01:27:06 +0300

    search bar text araması fonksiyonel hale getirildi, enable kontrolü ile iki farklı durum eklendi, lokasyon ve en yakın parklar için http requestleri yapıldı
    arama fonksiyonu içeriği eklendi, içerikte texten aramsından öncelikle place id sonra place info bilgileri çekildi, bu bilgilerle destinationa en yakın boş parklar verisi çekildi Bu veriler singleton yapılarında tutuldu. Journey başlangıcı sonrası search bar kullanılamaması için inactive hale getiren koşullar ve bu koşullarda ekrana çıkacak dialoglar belirlendi.

--- lib/utils/searchBar.dart
+++ lib/utils/searchBar.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:park_guide_istanbul/pages/nearestParks.dart';
+import 'package:park_guide_istanbul/patterns/Singleton.dart';
+import 'package:park_guide_istanbul/patterns/config.dart';
+import 'package:park_guide_istanbul/patterns/httpReqs.dart';
+import 'package:park_guide_istanbul/patterns/maps.dart';
+import 'package:park_guide_istanbul/patterns/objects.dart';
+import 'package:park_guide_istanbul/patterns/userLocation.dart';
+import 'package:park_guide_istanbul/utils/helper.dart';
 import 'package:park_guide_istanbul/utils/ui_features.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MainSearchBar extends StatefulWidget {
   MainSearchBar({Key? key}) : super(key: key);
@@ -16,11 +26,124 @@ class _MainSearchBarState extends State<MainSearchBar> {
   TextEditingController _searchBarController = TextEditingController();
 
   void _searchFromBar() {
-    String place = _searchBarController.text;
-    print(place);
+    if (Config.getOnJourney()) {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)),
+                actionsAlignment: MainAxisAlignment.spaceEvenly,
+                title: Text(
+                  'You have a journey.',
+                  style: CustomTextStyles.drawerMenuTextStyle(),
+                ),
+                content: Text(
+                    'You should stop the journey to look for new destinations.',
+                    style: TextStyle(color: CustomColors.middlePurple())),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(); // Close the AlertDialog
+                    },
+                    child: Text('OK'),
+                  )
+                ],
+              ));
+      return;
+    }
+
+    String adres = _searchBarController.text;
+    String id = '';
+    String dist = '';
+    double lat = 0.0;
+    double lng = 0.0;
+    showDialog(
+        context: context,
+        builder: (context) => Center(child: CircularProgressIndicator()));
+
+    _mapsApi.getIdFromAddress(adres).then(((value) {
+      print(value);
+      id = value;
+      print('PLACE ID: ' + id);
+      _mapsApi.getCoordinatesFromAddress(id).then(
+        (value) {
+          print("LOCATION INFO: \n");
+          print(value['district']);
+
+          String district = '';
+          district = Helpers.correctDistrictName(value['district']);
+
+          dist = district;
+          lat = value['latitude'];
+          lng = value['longitude'];
+          Map<String, dynamic> data = {
+            "district": district,
+            "lat": "${value['latitude']}",
+            "lng": "${value['longitude']}"
+          };
+          httpReq.nearestParksPostRequest(data).then((value) {
+            /*for (int i = 0; i < value.length; i++) {
+              print(
+                  'PARK ID: ${value[i]['parkID']}, PARK NAME: ${value[i]['parkName']}\n');
+            }*/
+
+            ParkListSingleton.getInstance().setList(parkList: [
+              Park(
+                  name: value[0]['parkName'],
+                  district: value[0]['district'],
+                  latitude: double.parse(value[0]['lat']),
+                  longitude: double.parse(value[0]['lng']),
+                  mapsURL: value[0]['MapsURL'],
+                  capacity: value[0]['capacity'],
+                  emptyCapacity: value[0]['emptyCapacity']),
+              Park(
+                  name: value[1]['parkName'],
+                  district: value[1]['district'],
+                  latitude: double.parse(value[1]['lat']),
+                  longitude: double.parse(value[1]['lng']),
+                  mapsURL: value[1]['MapsURL'],
+                  capacity: value[1]['capacity'],
+                  emptyCapacity: value[1]['emptyCapacity']),
+              Park(
+                  name: value[2]['parkName'],
+                  district: value[2]['district'],
+                  latitude: double.parse(value[2]['lat']),
+                  longitude: double.parse(value[2]['lng']),
+                  mapsURL: value[2]['MapsURL'],
+                  capacity: value[2]['capacity'],
+                  emptyCapacity: value[2]['emptyCapacity']),
+              Park(
+                  name: value[3]['parkName'],
+                  district: value[3]['district'],
+                  latitude: double.parse(value[3]['lat']),
+                  longitude: double.parse(value[3]['lng']),
+                  mapsURL: value[3]['MapsURL'],
+                  capacity: value[3]['capacity'],
+                  emptyCapacity: value[3]['emptyCapacity']),
+              Park(
+                  name: value[4]['parkName'],
+                  district: value[4]['district'],
+                  latitude: double.parse(value[4]['lat']),
+                  longitude: double.parse(value[4]['lng']),
+                  mapsURL: value[4]['MapsURL'],
+                  capacity: value[4]['capacity'],
+                  emptyCapacity: value[4]['emptyCapacity'])
+            ]);
+            LocationSingleton locationSingleton =
+                LocationSingleton.getInstance();
+            locationSingleton.setSearchAsCurrentLocation(false);
+            locationSingleton.setSearchedLocation(adres);
+            locationSingleton.setSearchedLocationDistrict(dist);
+            locationSingleton.setSearchedLat(lat);
+            locationSingleton.setSearchedLng(lng);
+            Navigator.of(context).pop();
+            Navigator.of(context).push(
+                CupertinoPageRoute(builder: (context) => NearestParksPage()));
+          });
+        },
+      );
+    }));
   }
-
-
 
   void _selfLocation() {
     if (Config.getOnJourney()) {
@@ -129,22 +252,17 @@ class _MainSearchBarState extends State<MainSearchBar> {
       });
     });
   }
-  
 
-  
-
-
-
-
-  
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.bottomCenter,
       padding: const EdgeInsets.all(32),
       child: CupertinoSearchTextField(
+        enabled: Config.getSearchBarEnable(),
         backgroundColor: CustomColors.customGrey(),
-        prefixIcon: Icon(CupertinoIcons.map),
+        prefixIcon: GestureDetector(
+            onTap: _selfLocation, child: Icon(Icons.my_location_sharp)),
         suffixIcon: Icon(CupertinoIcons.search),
         onSuffixTap: _searchFromBar,
         padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
